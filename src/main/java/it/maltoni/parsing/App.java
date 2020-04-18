1a88c05e19e56ab240a9e57d797e608ef23eab01
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2020-04-18 15:34:19 +0200

    style: minor code refactor

--- src/main/java/it/maltoni/parsing/App.java
+++ src/main/java/it/maltoni/parsing/App.java
@@ -6,16 +6,16 @@ import org.antlr.v4.runtime.CommonTokenStream;
 
 public class App {
 
-    public static void main(String[] args) {
-        CharStream inputStream = CharStreams.fromString(
+    public static void main(final String[] args) {
+        final CharStream inputStream = CharStreams.fromString(
             "I would like to [b]emphasize[/b] this and [u]underline [b]that[/b][/u]. " +
                 "Let's not forget to quote: [quote author=\"John\"]You're wrong![/quote]");
-        MarkupLexer markupLexer = new MarkupLexer(inputStream);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(markupLexer);
-        MarkupParser markupParser = new MarkupParser(commonTokenStream);
+        final MarkupLexer markupLexer = new MarkupLexer(inputStream);
+        final CommonTokenStream commonTokenStream = new CommonTokenStream(markupLexer);
+        final MarkupParser markupParser = new MarkupParser(commonTokenStream);
 
-        MarkupParser.FileContext fileContext = markupParser.file();
-        MarkupVisitor visitor = new MarkupVisitor();
+        final MarkupParser.FileContext fileContext = markupParser.file();
+        final MarkupVisitor visitor = new MarkupVisitor();
         visitor.visit(fileContext);
     }
 }
--- src/main/java/it/maltoni/parsing/MarkupVisitor.java
+++ src/main/java/it/maltoni/parsing/MarkupVisitor.java
@@ -1,19 +1,17 @@
 package it.maltoni.parsing;
 
+/** Our visitor prints all the text and ignore all the tags. */
 public class MarkupVisitor extends MarkupParserBaseVisitor<String> {
     @Override
-    public String visitFile(MarkupParser.FileContext context) {
+    public String visitFile(final MarkupParser.FileContext context) {
         visitChildren(context);
-
         System.out.println();
-
         return null;
     }
 
     @Override
-    public String visitContent(MarkupParser.ContentContext context) {
+    public String visitContent(final MarkupParser.ContentContext context) {
         System.out.print(context.TEXT().getText());
-
         return visitChildren(context);
     }
 }
